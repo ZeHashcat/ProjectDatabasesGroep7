2a3f3227b75b898320357d04ba7a8080e8123bc0
ZeHashcat <Mylo.bronkhorst@live.com>
2022-02-16 21:58:50 +0100

    Test C# project. Displays a message and contains a class.

---
+++ GitHubTestOne/GitHubTestSolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GitHubTestSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            test.Start();
+        }
+
+        void Start()
+        {
+            TestClass testClass = new TestClass("Konichiwa worurudo!!!");
+            Console.WriteLine(testClass.HerroWorldu);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GitHubTestOne/GitHubTestSolution/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestSolution
+{
+    public class TestClass
+    {
+        public string HerroWorldu;
+
+        public TestClass(string herroWorldu)
+        {
+            HerroWorldu = herroWorldu;
+        }
+    }
+}
